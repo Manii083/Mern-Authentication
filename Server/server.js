e71bf0c548e540a44b71af57cc1e165bf89e4ce4
Mani <manipatelhindu@gmail.com>
2025-10-07 16:31:54 +0530

    create server and connect db

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send("Hello from Server");
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((error) => console.log("❌ MongoDB Connection Failed:", error.message));
 
-app.listen(5000, () => {
-  console.log("Server is running on Port 5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+// app.listen(PORT, () => {
+//   console.log("Server is running on Port 5000");
+// });
