874cd99b5a6e3c64ce6b10ddc06150260ce0444f
Mani <manipatelhindu@gmail.com>
2025-10-07 19:33:07 +0530

    change type to module

---
+++ Server/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MongoDB Connected Successfully ${conn.connection.host}`);
+  } catch (error) {
+    console.log("❌ MongoDB Connection Failed:", error.message);
+    process.exit(1);
+  }
+}
--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const dotenv = require('dotenv');
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
 dotenv.config();
+const app = express();
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected Successfully"))
-  .catch((error) => console.log("❌ MongoDB Connection Failed:", error.message));
 
+connectDB();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-// app.listen(PORT, () => {
-//   console.log("Server is running on Port 5000");
-// });
