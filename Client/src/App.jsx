380eb7ec2571c9b221d4df9f5c9072229a037e9b
Mani <manipatelhindu@gmail.com>
2025-10-08 14:08:51 +0530

    create components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter as Router , Routes, Route, Navigate} from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [eroor, setError] = useState("");
+
+useEffect(() => {
+  const fetchUser = async () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const res = await axios.get("/api/users/me", {
+          Headers: { Authorization: `Bearer ${token}`},
+        });
+        setUser(res.data);
+      } catch (error) {
+        setError("Failed to fetch user data");
+        localStorage.removeItem("token");
+      }
+    }
+  };
+  fetchUser();
+}, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      < Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ Client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  const [error, setError] = useState("");
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-blue-100'>
+      <div className='bg-white p-8 rounded-lg shodow-lg w-full max-w-md border border-gray-200'></div>
+      Login</div>
+  )
+}
+
+export default Login
