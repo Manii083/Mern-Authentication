b019f6ff2e292325a117238c9512faff9fcbdd5a
Mani <manipatelhindu@gmail.com>
2025-10-09 13:49:24 +0530

    update server

--- Server/server.js
+++ Server/server.js
@@ -17,4 +17,4 @@ connectDB();
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));   
